a87473630a5f75d4867b0076c747bd4f6ee70dbd
hanezawa <nathan.gamasms@gmail.com>
2023-08-07 03:31:25 -0500

    Login Y registro Funcional

--- administrador/back/datosRegistro.php
+++ administrador/back/datosRegistro.php
@@ -8,25 +8,14 @@ $user = $_POST['user'];
 $mail = $_POST['email'];
 $pass = $_POST['pass'];
 $passConfirm = $_POST['passConfirm'];
-<<<<<<< HEAD
 $passEncrypted = password_hash($pass,PASSWORD_DEFAULT);
     if($pass === $passConfirm){
         $consultRegister = "INSERT INTO usuarios (usuario, email, password) VALUES ('$user','$mail', '$passEncrypted')";
-=======
-    if([$pass === $passConfirm]){
-        $consultRegister = "INSERT INTO usuarios (user, email, password) VALUES ('$user','$mail', '$passEncrypted')";
->>>>>>> 4458b4c644a1989125d838ce3bd957b12192ff12
-        $queryRegister = mysqli_query($conn, $consultRegister);
 
         if($queryRegister){
             $register_success = true;
-<<<<<<< HEAD
             header('location: ../../index.php');
-        }else{  
-=======
-            return $register_success;
-        } else{
->>>>>>> 4458b4c644a1989125d838ce3bd957b12192ff12
+        }else{
             $register_success = false;
         }
     } else{
